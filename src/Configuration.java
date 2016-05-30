c8124238b43c5ecbd56e751d18b3749a8f5435d2
Timothy Geary <tdgeary@gmail.com>
2016-05-30 00:34:12 -0400

    Removed unnecessary public keywords for the interface methods

--- src/Configuration.java
+++ src/Configuration.java
@@ -10,17 +10,17 @@ interface Configuration{
      * Get an arrayList of successors from the current configuration.
      * @return All successors, they can even be invalid
      */
-    public ArrayList<Configuration> getSuccessors();
+    ArrayList<Configuration> getSuccessors();
 
     /**
      * Check if the current configuration is valid by the puzzle's rules
      * @return true if valid and false otherwise
      */
-    public boolean isValid();
+    boolean isValid();
 
     /**
      * Check if the current configuration is the goal being searched for
      * @return true if goal and false otherwise
      */
-    public boolean isGoal();
+    boolean isGoal();
 }
