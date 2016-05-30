a1a51d859f5c41e1d3fb697c7874755f2bce9973
Timothy Geary <tdgeary@gmail.com>
2016-05-30 00:32:35 -0400

    Fleshed out the skeleton code

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 /**
  * Solves either an n Queen problem, using a given n that's an
  * integer between one and eight, inclusive, and times how long
@@ -7,16 +9,51 @@
  */
 public class NQueensSolver{
     public static void main(String[] args){
-        if(args.length == 0){
-            //todo - solve 1-8, inclusive
-        }else if(args.length == 1){
-            if(args[0].matches("[1-8]")){
+        if(args.length == 0){//solve all eight n problems
+            for(int n=1; n < 9; n++){
+                if(n==1) System.out.println("Solving the 1 Queen problem:\n");
+                else System.out.println("Solving the "+n+" Queens problem:\n");
+
+                double start = System.currentTimeMillis();
+
+                Configuration init = new NQueensConfig(n);
+                Backtracker bt = new Backtracker();
+
+                Optional<Configuration> sol = bt.solve(init);
+
+                System.out.println("Solved in: " + (System.currentTimeMillis() - start)/1000.0 + " seconds.");
+
+                if(sol.isPresent()){
+                    System.out.println("Solution:\n" + sol.get() + "\n");
+                }else{
+                    System.out.println("No solution found.\n");
+                }
+            }
+        }else if(args.length == 1){//only solve one n problem
+            if(args[0].matches("[1-8]")){//make sure the user provided a valid n to use
                 int n = Integer.parseInt(args[0]);
-                //todo - solve the n problem
-            }else{
+
+                if(n==1) System.out.println("Solving the 1 Queen problem:\n");
+                else System.out.println("Solving the "+n+" Queens problem:\n");
+
+                double start = System.currentTimeMillis();
+
+                Configuration init = new NQueensConfig(n);
+                Backtracker bt = new Backtracker();
+
+                Optional<Configuration> sol = bt.solve(init);
+
+                System.out.println("Solved in: " + (System.currentTimeMillis() - start)/1000.0 + " seconds.");
+
+                if(sol.isPresent()){
+                    System.out.println("Solution:\n" + sol.get());
+                }else{
+                    System.out.println("No solution found.");
+                }
+            }else{//invalid n given by the user
                 System.out.println("Usage: n must be an integer between one and eight, inclusive");
             }
-        }else{
+        }else{//user provided too many arguments
             System.out.println("Usage: java NQueensSolver n");
         }
     }
